6939550cd051d8bc76ddebf3e2b968ed309b1e86
ThisIsKhalid <iamkhalid.hs@gmail.com>
2023-11-15 13:34:41 +0600

    completed resusable modal, added resusable btn

--- src/components/modals/Modal.tsx
+++ src/components/modals/Modal.tsx
@@ -1,14 +1,18 @@
 "use client";
+
 import { useCallback, useEffect, useState } from "react";
+import { IoMdClose } from "react-icons/io";
+
+import Button from "../Button";
 
 interface ModalProps {
   isOpen?: boolean;
-  onClose?: () => void;
-  onSubmit?: () => void;
+  onClose: () => void;
+  onSubmit: () => void;
   title?: string;
   body?: React.ReactElement;
   footer?: React.ReactElement;
-  actionLabel?: string;
+  actionLabel: string;
   disabled?: boolean;
   secondaryAction?: () => void;
   secondaryActionLabel?: string;
