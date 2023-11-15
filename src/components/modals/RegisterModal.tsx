dc8cb269738b2238d8bb21a2fbb7470acf41ada5
ThisIsKhalid <iamkhalid.hs@gmail.com>
2023-11-15 14:32:18 +0600

    added reusable input and heading cmponent

--- src/components/modals/RegisterModal.tsx
+++ src/components/modals/RegisterModal.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { closeModal } from "@/redux/features/modals/useRegisterSlice";
 import { useAppDispatch, useAppSelector } from "@/redux/hooks";
 import axios from "axios";
 import { useCallback, useState } from "react";
@@ -7,8 +8,9 @@ import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import { toast } from "react-hot-toast";
 import { AiFillGithub } from "react-icons/ai";
 import { FcGoogle } from "react-icons/fc";
+import Heading from "../Heading";
+import Input from "../inputs/Input";
 import Modal from "./Modal";
-import { closeModal } from "@/redux/features/modals/useRegisterSlice";
 
 const RegisterModal = () => {
   const dispatch = useAppDispatch();
@@ -30,30 +32,61 @@ const RegisterModal = () => {
   const onSubmit: SubmitHandler<FieldValues> = (data) => {
     setIsLoading(true);
 
-     axios
-       .post("/api/register", data)
-       .then(() => {
-         toast.success("Registered!");
-         dispatch(closeModal());
-       })
-       .catch((error) => {
-         toast.error(error);
-       })
-       .finally(() => {
-         setIsLoading(false);
-       });
+    axios
+      .post("/api/register", data)
+      .then(() => {
+        toast.success("Registered!");
+        dispatch(closeModal());
+      })
+      .catch((error) => {
+        toast.error(error);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
   };
-//   console.log(isOpen);
+  //   console.log(isOpen);
 
-    
+  const bodyContent = (
+    <div className="flex flex-col gap-4">
+      <Heading title="Welcome to Travel Nest" subtitle="Create an account!" />
+      <Input
+        id="email"
+        label="Email"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+      <Input
+        id="name"
+        label="Name"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+      <Input
+        id="password"
+        label="Password"
+        type="password"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+    </div>
+  );
 
   return (
-    <Modal 
-     disabled={isLoading}
-        isOpen={isOpen}
-        title="Register"
-        onClose={() => dispatch(closeModal())}
-        onSubmit={handleSubmit(onSubmit)}
+    <Modal
+      disabled={isLoading}
+      isOpen={isOpen}
+      title="Register"
+      onClose={() => dispatch(closeModal())}
+      onSubmit={handleSubmit(onSubmit)}
+      body={bodyContent}
+      actionLabel="Register"
     />
   );
 };
