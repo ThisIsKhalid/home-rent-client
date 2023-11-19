01a8a9b84b8834a8b0fea8a10335f6424816328d
ThisIsKhalid <iamkhalid.hs@gmail.com>
2023-11-19 08:24:02 +0600

    completed rent modal Price(Step6)

--- src/components/modals/RentModal.tsx
+++ src/components/modals/RentModal.tsx
@@ -4,15 +4,16 @@ import { closeRentModal } from "@/redux/features/modals/useRentModalSlice";
 import { useAppDispatch, useAppSelector } from "@/redux/hooks";
 import dynamic from "next/dynamic";
 import { useMemo, useState } from "react";
-import { FieldValues, useForm } from "react-hook-form";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import toast from "react-hot-toast";
 import Heading from "../Heading";
 import CategoryInput from "../inputs/CategoryInput";
-import CountrySelect from "../inputs/CountrySelect";
-import { categories } from "../navbar/Categories";
-import Modal from "./Modal";
 import Counter from "../inputs/Counter";
+import CountrySelect from "../inputs/CountrySelect";
 import ImageUpload from "../inputs/ImageUpload";
 import Input from "../inputs/Input";
+import { categories } from "../navbar/Categories";
+import Modal from "./Modal";
 
 enum STEPS {
   CATEGORY = 0,
@@ -80,6 +81,30 @@ const RentModal = () => {
 
   const onNext = () => {
     setStep((value) => value + 1);
+  };
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    if (step !== STEPS.PRICE) {
+      return onNext();
+    }
+  console.log(data);
+    // setIsLoading(true);
+
+    // axios
+    //   .post("/api/listings", data)
+    //   .then(() => {
+    //     toast.success("Listing created!");
+    //     router.refresh();
+    //     reset();
+    //     setStep(STEPS.CATEGORY);
+    //     rentModal.onClose();
+    //   })
+    //   .catch(() => {
+    //     toast.error("Something went wrong.");
+    //   })
+    //   .finally(() => {
+    //     setIsLoading(false);
+    //   });
   };
 
   const actionLabel = useMemo(() => {
@@ -191,41 +216,62 @@ const RentModal = () => {
     );
   }
 
-   if (step === STEPS.DESCRIPTION) {
-     bodyContent = (
-       <div className="flex flex-col gap-8">
-         <Heading
-           title="How would you describe your place?"
-           subtitle="Short and sweet works best!"
-         />
-         <Input
-           id="title"
-           label="Title"
-           disabled={isLoading}
-           register={register}
-           errors={errors}
-           required
-         />
-         <hr />
-         <Input
-           id="description"
-           label="Description"
-           disabled={isLoading}
-           register={register}
-           errors={errors}
-           required
-         />
-       </div>
-     );
-   }
+  if (step === STEPS.DESCRIPTION) {
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading
+          title="How would you describe your place?"
+          subtitle="Short and sweet works best!"
+        />
+        <Input
+          id="title"
+          label="Title"
+          disabled={isLoading}
+          register={register}
+          errors={errors}
+          required
+        />
+        <hr />
+        <Input
+          id="description"
+          label="Description"
+          disabled={isLoading}
+          register={register}
+          errors={errors}
+          required
+        />
+      </div>
+    );
+  }
+
+  if (step === STEPS.PRICE) {
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading
+          title="Now, set your price"
+          subtitle="How much do you charge per night?"
+        />
+        <Input
+          id="price"
+          label="Price"
+          formatPrice
+          type="number"
+          disabled={isLoading}
+          register={register}
+          errors={errors}
+          required
+        />
+      </div>
+    );
+  }
 
   return (
     <Modal
-      //   disabled={isLoading}
+      disabled={isLoading}
       isOpen={isOpen}
       title="Airbnb your home!"
       actionLabel={actionLabel}
-      onSubmit={() => onNext()}
+      onSubmit={handleSubmit(onSubmit)}
       secondaryActionLabel={secondaryActionLabel}
       secondaryAction={step === STEPS.CATEGORY ? onNext : onBack}
       onClose={() => dispatch(closeRentModal())}
